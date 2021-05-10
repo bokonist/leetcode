5cc4a3c29eecad0340b6fec92fdba4ed220859a0
bokonist <jayrm1995@gmail.com>
2021-05-10 13:25:19 +0530

    9 palindrome

---
+++ 9.palindrome-number.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  if (x === 0) return true;
+  else {
+    let i = x,
+      arr = [];
+    while (i > 0) {
+      arr.unshift(i % 10);
+      i = Math.floor(i / 10);
+    }
+    let pointer = 0;
+    let condition = true;
+    let result = false;
+    let length = arr.length;
+    while (condition) {
+      if (pointer === length - 1 - pointer) {
+        //middle element
+        return true;
+      }
+      if (pointer === length - 1 - pointer - 1) {
+        //middle 2 elements
+        if (arr[pointer] === arr[pointer + 1]) return true;
+      }
+      if (arr[pointer] === arr[length - 1 - pointer]) {
+        pointer++;
+        continue;
+      }
+      return false;
+    }
+    return result;
+  }
+};
+// @lc code=end
+
+// @after-stub-for-debug-begin
+module.exports = isPalindrome;
+// @after-stub-for-debug-end
